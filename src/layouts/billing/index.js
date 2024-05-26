41ff44f164755330182583e61c678c2a83564783
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-25 20:51:13 -0500

    feat: implement crypto currency trackers

--- src/layouts/billing/index.js
+++ src/layouts/billing/index.js
@@ -13,7 +13,7 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MDBox from "components/MDBox";
 import Grid from "@mui/material/Grid";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
@@ -52,73 +52,108 @@ function Billing() {
     setCustomTitle("Solicitar ingreso/retiro")
     setCustomDescription("Ingresa la información de transacción")
   };
+
+  useEffect(() => {
+    const currencies = ["COINBASE:BTCUSDT", "COINBASE:ETHUSDT", "OANDA:EURUSD"]
+    for(const currency of currencies) {
+      const script = document.createElement('script');
+      script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js';
+      script.async = true;
+      script.innerHTML = JSON.stringify({
+        "symbol": currency,
+        "width": "100%",
+        "height": "100%",
+        "locale": "en",
+        "dateRange": "12M",
+        "colorTheme": "light",
+        "isTransparent": true,
+        "autosize": true,
+        "largeChartUrl": ""
+      });
+      document.getElementById('tradingview-widget').appendChild(script);
+    }
+
+    const scripts = ["https://files.coinmarketcap.com/static/widget/coinPriceBlock.js", "https://www.livecoinwatch.com/static/lcw-widget.js"]
+    for (let url of scripts) {
+      const script = document.createElement('script');
+      script.src = url;
+      script.defer = true;
+      document.body.appendChild(script);
+    }
+  }, []);
   
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox mt={4} mx={4}>
+        <MDBox mb={2}>
+          <MDTypography variant="body2">Podría interesarte:</MDTypography>
+          <div className="livecoinwatch-widget-5" lcw-base="USD" lcw-color-tx="#999999" lcw-marquee-1="coins" lcw-marquee-2="movers" lcw-marquee-items="10"></div>
+        </MDBox>
         <Grid container spacing={3} item xs={12} lg={12} alignItems="flex-start">
             { admin &&
-              <Grid container  spacing={3} item xs={12} xl={6}>
+              <Grid container  spacing={3} item xs={12} xl={8}>
                 <Grid item xs={12} xl={12}>
                   <Card>
                     <MDBox p={2} mx={3}>
-                      <MDTypography my={0} variant="h4" fontWeight="medium" textTransform="capitalize">Información de Depósito</MDTypography>
+                      <MDTypography my={0} variant="h5" fontWeight="medium" textTransform="capitalize">Información de Depósito</MDTypography>
                       <MDTypography variant="body2" color="text" fontWeight="regular">{admin.entity_name}</MDTypography>
                     </MDBox>
                   </Card>
                 </Grid>
                 <Grid container spacing={3} item xs={12} xl={12}>
-                  <Grid item xs={12}>
-                  <MasterCard
-                      color="warning"
-                      logo={admin.btc_qr}
-                      number={admin.btc_address || "---"}
-                      title="Dirección Bitcoin"
-                      holder={admin.entity_name}
-                      icon={faBitcoin}
-                      isFontAwesome
-                    />
+                  <Grid item xs={12} md={6}>
+                    <Grid container flexDirection={"column"} spacing={3}>
+                      <Grid item xs={12}>
+                        <MasterCard
+                            color="warning"
+                            logo={admin.btc_qr}
+                            number={admin.btc_address || "---"}
+                            title="Dirección Bitcoin"
+                            holder={admin.entity_name}
+                            icon={faBitcoin}
+                            isFontAwesome
+                          />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <MasterCard
+                          color="info"
+                          logo={admin.ethereum_qr}
+                          number={admin.ethereum_address || "---"}
+                          title="Dirección Ethereum"
+                          holder={admin.entity_name}
+                          icon={faEthereum}
+                          isFontAwesome
+                        />
+
+                      </Grid>
+
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12}>
-                    <MasterCard
-                      color="info"
-                      logo={admin.ethereum_qr}
-                      number={admin.ethereum_address || "---"}
-                      title="Dirección Ethereum"
-                      holder={admin.entity_name}
-                      icon={faEthereum}
-                      isFontAwesome
-                    />
+                  <Grid item xs={12} md={6}>
+                    <MDBox p={2}>
+                      <div id="tradingview-widget">
+                        <div class="tradingview-widget-container">
+                          <div class="tradingview-widget-container__widget"></div>
+                        </div>
+                      </div>                      
+                    </MDBox>
                   </Grid>
-                  {/* <DefaultInfoCard
-                    icon={faBitcoin}
-                    title="Dirección Bitcoin"
-                    picture={admin.btc_qr}
-                    value={admin.btc_address || "---"}
-                    isFontAwesome
-                  /> */}
-                  {/* <DefaultInfoCard
-                    icon={faEthereum}
-                    title="Dirección Ethereum"
-                    picture={admin.ethereum_qr}
-                    value={admin.ethereum_address || "---"}
-                    isFontAwesome
-                  /> */}
+                  
                 </Grid>
               </Grid>
             }
             {user.usd_wallet && user.i_wallet &&
-              <Grid container spacing={3} item xs={12} xl={6} alignItems="flex-start">
+              <Grid container spacing={3} item xs={12} xl={4} alignItems="flex-start">
                 <Grid item xs={12} xl={12}>
                   <Card >
                     <MDBox p={2} mx={3}>
-                      <MDTypography my={0} variant="h4" fontWeight="medium" textTransform="capitalize">Tus Tu Billeteras Activas</MDTypography>
+                      <MDTypography my={0} variant="h5" fontWeight="medium" textTransform="capitalize">Tus Billeteras Activas</MDTypography>
                       <MDTypography variant="body2" color="text" fontWeight="regular">{user.username}</MDTypography>
                     </MDBox>
                   </Card>
                 </Grid>
-                <Grid item xs={12} xl={6}>
+                <Grid item xs={12} xl={12}>
                   <DefaultInfoCard
                     icon="wallet"
                     title="Tu Billetera USDT (trc20)"
@@ -126,7 +161,7 @@ function Billing() {
                     value={`${formatCurrency(user.usd_wallet.available_amount)}`}
                   />
                 </Grid>
-                <Grid item xs={12} xl={6}>
+                <Grid item xs={12} xl={12}>
                   <DefaultInfoCard
                     icon="wallet"
                     title="Tu Billetera de Comercio"
