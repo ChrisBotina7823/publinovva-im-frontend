cd3231d9b24719044757d94d88ef1532b5ee9c14
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-27 17:31:28 -0500

    hotfix: change debug to false

--- src/axiosInstance/index.js
+++ src/axiosInstance/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export default () => {
   const token = localStorage.getItem("token");
   try {
-    let debug = true
+    let debug = false
     return axios.create({
       baseURL: debug ? "http://localhost:4000" : "https://publinovva-im-backend-production.up.railway.app",  // Asegúrate de ajustar la URL base según tu configuración
       headers: {
